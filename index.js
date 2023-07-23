bb616feacc22082c61ea946c3078bc482d9e9a32
Mayank <mayanksethi324@gmail.com>
2023-07-23 14:51:42 +0530

    added required dependencies and basic config

---
+++ index.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const path = require('path');
+const app = express();
+const port = 8008 || process.env.PORT;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); // <-- Corrected the line
+
+// Static files
+app.use(express.static('public'));
+
+// Template engine
+app.use(expressLayouts);
+app.set('layout', './layouts/main');
+// Set the views directory
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+app.get('/', function (req, res) {
+    const locals={
+        title:'Notes',
+        description:'Free Notes App'
+    }
+  res.render('index',locals);
+});
+
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`);
+});
